353a3440f164be298efcecc569be531e09521da7
Mertcan3457 <72562227+Mertcan3457@users.noreply.github.com>
2020-10-08 17:50:07 +0300

    Add files via upload

---
+++ Taş Kağıt Makas Oyunu/TaşKağıtMakasOyunu/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace TaşKağıtMakasOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(242, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Taş-Kağıt-Makas Oyunu ";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoCheck = false;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(16, 179);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 29);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Kağıt";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoCheck = false;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.Location = new System.Drawing.Point(9, 230);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(99, 29);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Makas";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(186, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "SEN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(534, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Bilgisayar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(310, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 33);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(386, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 33);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "---";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(473, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 33);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(315, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 45);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Tekrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(127, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(504, 129);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(177, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoCheck = false;
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton4.Location = new System.Drawing.Point(27, 129);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(62, 28);
+            this.radioButton4.TabIndex = 24;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Taş";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton4_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(315, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 45);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "Oyunu Sıfırla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(115, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 27;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Gold;
+            this.label7.Location = new System.Drawing.Point(12, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 25);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Adınız =";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(221, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 34);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "ONAYLA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gold;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(48, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(712, 291);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.BackgroundImage = global::TaşKağıtMakasOyunu.Properties.Resources.images;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(820, 417);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Taş Kağıt Makas Oyunu/TaşKağıtMakasOyunu/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaşKağıtMakasOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi;
+        int sayac = 0;
+        int skor1 = 0;
+        int skor2 = 0;
+        Random rastgele = new Random();
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            if (sayac % 3 == 0)
+            {
+                pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Taş;
+            }
+            else if (sayac % 3 == 1)
+            {
+                pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Kağıt;
+            }
+            else if (sayac % 3 == 2)
+            {
+                pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Makas;
+            }
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton4.Enabled = false;
+                radioButton3.Enabled = false;
+                pictureBox1.Visible = true;
+                timer1.Stop();
+                pictureBox1.Image = TaşKağıtMakasOyunu.Properties.Resources.Kağıt;
+                sayi = rastgele.Next(1, 4);
+                if (sayi == 1)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Taş;
+                }
+                else if (sayi == 2)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Kağıt;
+                }
+                else if (sayi == 3)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Makas;
+                }
+                if (2 == sayi)
+                {
+                    MessageBox.Show("Berabere Puan Verilemedi", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (2 < sayi)
+                {
+                    skor2++;
+                    label6.Text = skor2.ToString();
+                    MessageBox.Show("Makas Kağıdı Keser\nBilgisayar 1 Puan Kazandı","Yönetici Tarafından Bilgilendirme",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                else if (2 > sayi)
+                {
+                    skor1++;
+                    label4.Text = skor1.ToString();
+                    MessageBox.Show("Kağıt Taşı Sarar\n"+textBox1.Text+" 1 Puan Kazandı", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                radioButton4.Enabled = false;
+                radioButton2.Enabled = false;
+                pictureBox1.Visible = true;
+                timer1.Stop();
+                pictureBox1.Image = TaşKağıtMakasOyunu.Properties.Resources.Makas;
+                sayi = rastgele.Next(1, 4);
+                if (sayi == 1)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Taş;
+                }
+                else if (sayi == 2)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Kağıt;
+                }
+                else if (sayi == 3)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Makas;
+                }
+                if (3 == sayi)
+                {
+                    MessageBox.Show("Berabere Puan Verilemedi", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (sayi < 2 && sayi < 3)
+                {
+                    skor2++;
+                    label6.Text = skor2.ToString();
+                    MessageBox.Show("Taş Makası Kırar\nBilgisayar 1 Puan Kazandı", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (sayi < 3 && sayi > 1)
+                {
+                    skor1++;
+                    label4.Text = skor1.ToString();
+                    MessageBox.Show("Makas Kağıdı Keser\n"+textBox1.Text+" 1 Puan Kazandı", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                radioButton2.Enabled = false;
+                radioButton3.Enabled = false;
+                pictureBox1.Visible = true;
+                timer1.Stop();
+                pictureBox1.Image = TaşKağıtMakasOyunu.Properties.Resources.Taş;
+                sayi = rastgele.Next(1, 4);
+                if (sayi == 1)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Taş;
+                }
+                else if (sayi == 2)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Kağıt;
+                }
+                else if (sayi == 3)
+                {
+                    pictureBox2.Image = TaşKağıtMakasOyunu.Properties.Resources.Makas;
+                }
+                if (1 == sayi)
+                {
+                    MessageBox.Show("Berabere Puan Verilemedi", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (1 < sayi && sayi < 3)
+                {
+                    skor2++;
+                    label6.Text = skor2.ToString();
+                    MessageBox.Show("Kağıt Taşı Sarar\nBilgisayar 1 Puan Kazandı", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (1 < sayi && 2 < sayi)
+                {
+                    skor1++;
+                    label4.Text = skor1.ToString();
+                    MessageBox.Show("Taş Makası Kırar\n"+textBox1.Text+" 1 Puan Kazandı", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            radioButton4.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            if (skor1 == 5 || skor2 == 5) 
+            {
+                radioButton2.Enabled = false;
+                radioButton3.Enabled = false;
+                radioButton4.Enabled = false;
+                button1.Visible = false;
+                button3.Visible = true;
+            }
+            radioButton4.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            pictureBox1.Visible = true;
+            sayac = 0;
+            timer1.Start();
+            if (skor1 == 5)
+            {
+                timer1.Stop();
+                MessageBox.Show("Tebrikler "+textBox1.Text+"\nOyunu Kazandınız","Yönetici Tarafından Bilgilendirme",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else if (skor2 == 5)
+            {
+                timer1.Stop();
+                MessageBox.Show("Malesef Bilgisayar Oyunu Kazandı", "Yönetici Tarafından Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Error);            
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            button3.Visible = false;
+            radioButton4.AutoCheck= true;
+            radioButton2.AutoCheck= true;
+            radioButton3.AutoCheck = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            radioButton4.Enabled = true;
+            if (skor1 == 5 || skor2 == 5)
+            {
+                button3.Text = "Oyunu Sıfırla";
+                button3.Visible = false;
+                button1.Visible = true; 
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+                skor1 = 0;
+                skor2 = 0;
+                label4.Text = skor1.ToString();
+                label6.Text = skor2.ToString();
+                sayac = 0;
+                timer1.Start();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Oyun Başlıyor İyi Şanslar");
+            timer1.Start();
+            label7.Visible = false;
+            button2.Visible = false;
+            textBox1.Visible = false;
+            groupBox1.Visible = true;
+            label2.Text = textBox1.Text;
+        }
+    }
+}
